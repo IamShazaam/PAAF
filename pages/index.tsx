11fe4ce6acf14bb8f256442ee7bad631b1a2066e
Jorge Pereda <jorge.pereda@chasacademy.se>
2023-05-23 01:51:43 +0200

    search minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,21 +13,51 @@ export default function Home() {
   const [fetchUrl, setFetchUrl] = useState<string>('/api/games');
   const [searchTerm, setSearchTerm] = useState<string>('');
 
+  // PAAC Standard
+  // useEffect(() => {
+  //   (async () => {
+  //     const games = await fetch(fetchUrl).then((res) => res.json());
+  //     setGames(games);
+  //   })();
+  // }, [games, fetchUrl]);
+
+  // Jorge's fix 
   useEffect(() => {
     (async () => {
       const games = await fetch(fetchUrl).then((res) => res.json());
       setGames(games);
     })();
-  }, [games, fetchUrl]);
+  }, [fetchUrl]);
   
+  // PAAF Standard
+  // useEffect(() => {
+  //   if (searchTerm == null || searchTerm.trim() === '') {
+  //     setFetchUrl('/api/games');
+  //   } else {
+  //     setFetchUrl(`/api/games?search=${searchTerm}`);
+  //     console.log(`Searching via ${fetchUrl}`)
+  //   }
+  // }, [fetchUrl, searchTerm]);
+
+  // Jorge's fix
   useEffect(() => {
     if (searchTerm == null || searchTerm.trim() === '') {
       setFetchUrl('/api/games');
     } else {
-      setFetchUrl(`/api/games?search=${searchTerm}`);
-      console.log(`Searching via ${fetchUrl}`)
+      setFetchUrl(`/api/games?search=${encodeURIComponent(searchTerm)}`);
     }
-  }, [fetchUrl, searchTerm]);
+  }, [searchTerm]);
+  
+  // Jorge's fix
+  const uniqueGames = games.reduce((unique, game) => {
+    if (!unique.find((g) => g.title === game.title)) {
+      unique.push(game);
+    }
+    return unique;
+  }, []);
+  
+  const gameCount = searchTerm.length > 0 ? uniqueGames.length : games.length;
+
 
   return (
     <>
@@ -38,7 +68,12 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <Search onSearch={setSearchTerm} />
-        {searchTerm.length && <span>Found {games.length} games</span>}
+
+        {/* PAAF Standard         */}
+        {/* {searchTerm.length && <span>Found {games.length} games</span>} */}
+
+        {/* Jorge's fix */}
+        {searchTerm.length && <span>Found {gameCount} games</span>}
         <GameList games={games} />
       </main>
     </>
